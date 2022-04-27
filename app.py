7e1cc0b49eb74d6a6e99903031a6b2a76cf82793
Soroush Kaviani <90821955+srkaviani@users.noreply.github.com>
2022-04-27 13:53:10 +0430

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import time
 import requests
 
 
-temp=[{'metric': {'__name__': 'libvirt_domain_state_code', 'compute': 'tbz-sc2-compute3', 'domain': 'instance-0000620a', 'flavorName': 'plan 2', 'host': 'compute3.iranserver', 'instance': 'tbz-sc2-compute3.greenweb.local:9000', 'instanceId': '2406d72d-cdc2-4ee4-aa65-2a122f6e213236011', 'instanceName': 'testtt', 'job': 'prometheus-libvirt-exporter', 'projectId': '2782b1d5c49f45a0a8f7ad44dae62160', 'projectName': 'GreenwebCloud', 'stateDesc': 'unknown', 'userId': '6d9a01c2be6940269b850492d7a97ed1', 'userName': 'admin'}, 'value': [1650954378.601, '1']}]
-PROMETHEUS = 'http://172.20.8.10:9090/'
+temp=[{'metric': {'__name__': 'libvirt_domain_state_code', 'compute': 'test-compute3', 'domain': 'instance-xxxxxxx', 'flavorName': 'plan x', 'host': 'compute3.test', 'instance': 'test3.local:9000', 'instanceId': 'xxxxxx-xxxxx-4eex4-xxxx-xxxxxxxxxx', 'instanceName': 'testtt', 'job': 'libvirt-exporter', 'projectId': 'xxxxxxxxxxxxxxxx', 'projectName': 'myCloud', 'stateDesc': 'unknown', 'userId': 'xxxxxxxxxxxxxxxxxx', 'userName': 'test'}, 'value': [1650954378.601, '1']}]
+PROMETHEUS = 'http://PROMETHEUS.local:9090/'
 
 while(True):
    time.sleep(1)
@@ -16,8 +16,7 @@ while(True):
       pass
    else:
       temp=results
-      r = requests.post('https://api.gportal.app/check/service', json=results)
-      r = requests.post('https://webhook.site/ebebdb5e-498b-4d7b-91ea-174e1cbcd782?', json=results)
+      r = requests.post('https://my.webhook', json=results)
       for result in results:
          print(' {metric}: {value[1]}'.format(**result))
          with open("/opt/libvirt_state/instances.log", "a") as file_object:
